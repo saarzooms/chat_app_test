44e8d2146c77b82713cdc217a885aa6eba845c04
Shamsaagazarzoo M Alam <arzoo.rkcet@gmail.com>
2023-04-20 12:56:01 +0530

    Update server.js
    for connection and disconnection

--- server_app/server.js
+++ server_app/server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const io = require("socket.io")(server)
 
 const PORT = 3000
 
 app.get('/', (req,res)=>{
     res.send("Server is running")
+})
+
+//io events
+io.on('connection',(socket)=>{
+    console.log('new connection ');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    })
 })
 
 server.listen(PORT, ()=>{
